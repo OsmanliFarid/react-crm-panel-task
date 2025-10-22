b9e6c43e1a2e03e3e7305641754c2a48cadb45dc
OsmanliFarid <osmanliferid686@gmail.com>
2025-10-22 19:58:53 +0400

    I installed home img

--- src/Components/WeatherDetail/WeatherImage/index.tsx
+++ src/Components/WeatherDetail/WeatherImage/index.tsx
@@ -1,10 +1,13 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../../ContextStore/GlobalContext";
+import axios from "axios";
 
 const WeatherImage = () => {
-  const { WeatherData } = useContext(GlobalContext);
+  const { WeatherData, SearchText } = useContext(GlobalContext);
+  const [data, setdata] = useState([]);
+  const [date, setdate] = useState<Date | null>(null);
   let weatherImg = "/images/default.png";
-
+  const API_KEY = "53123f2f232fdc830f13c2068acda716";
   if (WeatherData?.weather?.[0]?.main) {
     switch (WeatherData.weather[0].main) {
       case "Rain":
@@ -28,6 +31,17 @@ const WeatherImage = () => {
         weatherImg = "../public/images/default.png";
     }
   }
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${SearchText}&appid=${API_KEY}&units=metric`
+      )
+      .then(({ data }) => {
+        setdata(data);
+        const now = new Date();
+        setdate(now);
+      });
+  }, [SearchText]);
 
   return (
     <>
@@ -38,8 +52,32 @@ const WeatherImage = () => {
           className="w-[50%]"
         />
       </div>
-      <div className="">
-        <h1></h1>
+      <div className="flex items-center flex-col">
+        <div className="mt-5  font-medium text-center text-white">
+          <h1 className="text-5xl">{WeatherData?.main.temp} °C</h1>
+          <h1 className="mt-3 text-3xl">
+            {WeatherData?.weather?.[0]?.description}
+          </h1>
+          <div className="mt-3 flex justify-center gap-x-3 items-center">
+            <div className="flex items-center text-2xl gap-x-2">
+              <h1>Max:</h1>
+              <h1>{WeatherData?.main.temp_max} °</h1>
+            </div>
+            <div className="flex items-center text-2xl gap-x-2">
+              <h1>Min:</h1>
+              <h1>{WeatherData?.main.temp_min} °</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center mt-8">
+        <img src="../public/images/house.png" alt="" />
+      </div>
+      <div className="bg-[#3E2D8F] h-50  w-[95%] mx-auto rounded-2xl">
+        <div className="">
+          <h1>Today</h1>
+          <h1>{date?.toLocaleString()}</h1>
+        </div>
       </div>
     </>
   );
